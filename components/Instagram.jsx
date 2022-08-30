a6248b3f2d01f204e8e95cf7b350bbf4ca3f812a
Joe Klakus <joeyk2k@hotmail.com>
2022-08-30 12:54:43 +0100

    removed contact page

--- components/Instagram.jsx
+++ components/Instagram.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import InstagramImg from './InstagramImg'
+import Link from 'next/link'
 
 
 const Instagram = ({images}) => {
   return (
     <div className='max-w-[1240px] mx-auto text-center py-24'>
         <p className='text-3xl font-bold'>Follow us on Instagram</p>
-        <p className='text-xl pb-4'>@littlesea</p>
+      <Link href="https://www.instagram.com/littlesea_caravan/">
+        <p className='text-2xl pb-4 hover:cursor-pointer hover:opacity-70'>@littlesea</p>
+    </Link>
     <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-2 p-4'>
             <InstagramImg socialImg={"/275732175_241467038113189_8935177250275147484_n.jpg"}/>
             <InstagramImg socialImg={"/275471047_1070011113548560_3448120782974078495_n.jpg"}/>
