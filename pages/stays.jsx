f76387726d55a29e91ae0404794e259ac1afbfd4
Joe Klakus <joeyk2k@hotmail.com>
2022-09-11 08:45:43 +0100

    checked photos

--- pages/stays.jsx
+++ pages/stays.jsx
@@ -13,8 +13,8 @@ const Stays = () => {
 			close to the seaside town of Weymouth, meaning plenty of opportunities
 			to eat ice cream and build sandcastles."
 			/>
-			<h2 className="font-bold text-2xl p-4">The Caravan</h2>
-			<p className="p-4">
+			<h2 className="font-bold text-2xl p-4 m-4">The Caravan</h2>
+			<p className="p-4 m-4">
 				Delightful, family friendly caravan, our is a lovely 2 bedroom, 6 berth, dog friendly holiday caravan on the Littlesea Haven Holiday Park. You will have the entire caravan for you and your family, ideal for you to have the space to make memories. This Caravan is our families van, which we hire out to families.
 
 				We hire the entire caravan for you, your family and fury animals to make memories together.
@@ -22,18 +22,27 @@ const Stays = () => {
 			<Photos 
 			mainImage={"/9444e91e-dec7-4d6c-9917-a50a80af977c.jpg"}
 			header={"Living Area"}
-			image1={"/4a64b9a0-1d40-4426-af49-431638283b97.jpg"}
-			image2={"/86079a3c-3f77-41ca-bf3b-a4112771f807.jpg"}
-			image3={"/82cb5e28-34b7-49ea-89d4-471c20678712.jpg"}
-			image4={"/270e4139-896d-423f-a1a3-c5dd14d877d7.jpg"}
+			image1={"/kitchen_thumb.jpg"}
+			image2={"/living_thumb2.jpg"}
+			image3={"/living_thumb.jpg"}
+			image4={"/kitchen_thumb2.jpg"}
 			/>
+			
 			<Photos 
 			mainImage={"/275479870_361931885941449_2329734142031350467_n.jpg"}
 			header={"Sleeps 6 People"}
-			image1={"/275574534_1030066490935027_781147433819565446_n.jpg"}
-			image2={"/275479870_361931885941449_2329734142031350467_n.jpg"}
-			image3={"/0363c8bd-6890-4879-b89a-552bf4e9fe26.jpg"}
-			image4={"/2aba04bd-6627-4a28-8689-7d98a270000bdfs.jpg"}
+			image1={"/bathroom_thumb.jpg"}
+			image2={"/275654129_287278310160713_5553281349045637634_n.jpg"}
+			image3={"/bedroom_thumb.jpg"}
+			image4={"/2aba04bd-6627-4a28-8689-7d98a270000bdfs_thumb.jpg"}
+			/>
+			<Photos 
+			mainImage={"/278075197_312388834337296_8826556924680014577_n.jpg"}
+			header={"Activities"}
+			image1={"/277979504_667783274489354_4614835511071287641_n.jpg"}
+			image2={"/activities_thumb.jpg"}
+			image3={"/activity_thumb.jpg"}
+			image4={"/278111153_1048985652629691_6184023553561585616_n.jpg"}
 			/>
 		</div>
 	);
