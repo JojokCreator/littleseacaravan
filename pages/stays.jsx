eefa1c7540d5a53dc2f97d1028c3d00d606a4db5
Joe Klakus <joeyk2k@hotmail.com>
2022-10-06 11:24:36 +0200

    text utupdated

--- pages/stays.jsx
+++ pages/stays.jsx
@@ -11,7 +11,7 @@ const Stays = () => {
 			/>
 			<h2 className="font-bold text-2xl p-4 m-4">The Caravan</h2>
 			<p className="p-4 m-4">
-				Delightful, family friendly caravan, our is a lovely 2 bedroom, 6 berth, dog friendly holiday caravan on the Littlesea Haven Holiday Park. This Caravan is our families van, which we hire out to families.
+				Delightful, family friendly caravan, our is a lovely 2 bedroom, 6 berth, dog friendly holiday caravan on the Littlesea Haven Holiday Park. This Caravan is our families van, which we hire out to families. We offer stays starting on any day of the week. There is a minimum 3 night stay.
 			</p>
 			<Photos
 				mainImage={"/9444e91e-dec7-4d6c-9917-a50a80af977c.jpg"}
@@ -22,7 +22,9 @@ const Stays = () => {
 				image4={"/kitchen_thumb2.jpg"}
 			/>
 			<p className="p-4 m-4">
-				We hire the entire caravan for you, your family and fury animals to make memories together.
+				The caravan has two bedrooms, one bathroom and a spacious open plan living area. It is perfect for families wanting to visit The surrounding area.
+				You have the whole caravan to yourself. There is a picnic bench outside as well as space for one car.
+				The kitchen is fully equipped. We are also able to provide bed linens on request.
 			</p>
 			<Photos
 				mainImage={"/275479870_361931885941449_2329734142031350467_n.jpg"}
@@ -32,12 +34,10 @@ const Stays = () => {
 				image3={"/bedroom_thumb.jpg"}
 				image4={"/2aba04bd-6627-4a28-8689-7d98a270000bdfs_thumb.jpg"}
 			/>
-		<p className="p-4 m-4">
-			Littlesea Holiday Park is brimming with things
-			to do and see! There are two heated indoor and outdoor pools. Plus
-			tennis, archery and a Natural Trail for budding explorers. The park is
-			close to the seaside town of Weymouth, meaning plenty of opportunities
-			to eat ice cream and build sandcastles.
+			<p className="p-4 m-4">
+				Littlesea has plenty to do for everyone including restaurants, takeaways and play areas which are available for everyone to use. There are also two pools, entertainment and lots activities available with the purchase of a haven play pass.
+				The area and the surrounding Jurassic coast has plenty to do. Coastal walks, beach days, castles and forts. There are plenty of places to hire water sports equipment nearby. The park is close to the seaside town of Weymouth, meaning plenty of opportunities
+				to eat ice cream and build sandcastles.
 			</p>
 			<Photos
 				mainImage={"/278075197_312388834337296_8826556924680014577_n.jpg"}
