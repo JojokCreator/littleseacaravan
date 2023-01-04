0196ba462c32bb3d7897d4c9a1c8d55dab3e345e
Joe Klakus <joeyk2k@hotmail.com>
2023-01-04 14:17:52 +0200

    analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,31 @@
-import '../styles/globals.css'
-import Navbar from '../components/Navbar'
-import Seo from '../components/Seo'
+import "../styles/globals.css";
+import Navbar from "../components/Navbar";
+import Seo from "../components/Seo";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  
-    return ( 
+  return (
     <>
-    <Seo />
-    <Navbar />
-    <Component {...pageProps} />
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-SBHDQ1210W`}
+      />
+
+      <Script id="googleanalytics" strategy="lazyOnload">
+        {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-SBHDQ1210W', {
+        page_path: window.location.pathname,
+      });
+  `}
+      </Script>
+      <Seo />
+      <Navbar />
+      <Component {...pageProps} />
     </>
-    )
+  );
 }
 
-export default MyApp
+export default MyApp;
