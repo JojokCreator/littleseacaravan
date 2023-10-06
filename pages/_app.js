4b1665ace89c2af242be670564e5ccadff828535
J_Klakus <98471049+JojokCreator@users.noreply.github.com>
2023-10-06 01:34:00 +0100

    Update _app.js
    Added goat counter

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,14 @@
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
 import Seo from "../components/Seo";
-import Script from "next/script";
+
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-SBHDQ1210W`}
-      />
-
-      <Script id="googleanalytics" strategy="lazyOnload">
-        {`
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-SBHDQ1210W', {
-        page_path: window.location.pathname,
-      });
-  `}
-      </Script>
+      <script data-goatcounter="https://littleseacaravan.goatcounter.com/count"
+        async src="//gc.zgo.at/count.js">
+      </script>
       <Seo />
       <Navbar />
       <Component {...pageProps} />
