22a4900a20a7aef014fa274007cf5cb93ce76fd8
J_Klakus <98471049+JojokCreator@users.noreply.github.com>
2023-10-06 01:46:46 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
-images: {
-unoptimized: true,
-},
-}
+    images: {
+      unoptimized: true,
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
